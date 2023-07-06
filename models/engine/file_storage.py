afb2cb339963572ac2cf81ccb6e5a7dc860302c8
Night8Guy <robert@lionsroad.com>
2023-07-05 21:52:26 -0500

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.amenity import Amenity
+from models.place_amenity import PlaceAmenity
 
 
 class FileStorage:
@@ -55,15 +57,19 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    if "__class__" in val and val["__class__"] == "Amenity":
+                        self.all()[key] = Amenity(**val)
+                    else:
+                        self.all()[key] = classes[val['__class__']](**val)
+
         except FileNotFoundError:
             pass
